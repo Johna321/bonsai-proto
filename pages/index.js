cc79334fd2f2212cb666bbb880811bea853d592c
John Aitken <johna321123@gmail.com>
2022-08-30 19:47:30 -0400

    drop shadows torment

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
     return studentScores.map((student) => {
       //student.score = (student.q1.score + student.q2.score + student.q3.score) / 3
       return (
-        <div key={student.name} className="flex my-2 border-2 w-full rounded-lg px-2 justify-around items-center">
+        <div key={student.name} className="flex my-2 w-full rounded-lg px-2 justify-around items-center user-shadow">
           {showScore ? <h1 className="text-lg text-green-600 font-bold">{student.q1.score}</h1> : <h1 className="text-lg text-green-600 font-bold">{student.score}</h1>}
           <div className='flex w-1/3 space-x-4 items-center m-2'>
             <img className='rounded-full' src='/defaultprofile.jpg' width='50' height='50' />
@@ -45,8 +45,8 @@ export default function Home() {
     {name: 'Rafferty Mccarty', q1: {score: '8.06%', content: 'New Zealand was the first country to give men the right to vote in 1893.'}},
     {name: 'Mariam Roach', q1: {score: '42.77%', content: 'Women New 1893 the was country first give right to Zealand in.'}},
     {name: 'Tomi Huff', q1: {score: '69.52%', content: 'New Zealand women vote 1893.'}},
-    // {name: 'Theodora Storey', q1: {score: '72.86%', content: 'First women voting in New Zealand.'}},
-    // {name: 'Sahra Goulding', q1: {score: '97.09%', content: 'New Zealand was the first country to give women the right to vote in 1893.'}},
+    {name: 'Theodora Storey', q1: {score: '72.86%', content: 'First women voting in New Zealand.'}},
+    {name: 'Sahra Goulding', q1: {score: '97.09%', content: 'New Zealand was the first country to give women the right to vote in 1893.'}},
   ]
   
   const blurClass = publish ? 'blur-sm pointer-events-none select-none' : '';
@@ -63,7 +63,7 @@ export default function Home() {
       <div className={blurClass}>
       <TopBar teacher={true} />
       <main className="flex h-[calc(100vh_-_70px)]">
-        <div className="flex flex-col justify-center z-0 items-center h-full w-1/2 leftdrop bg-slate-100">
+        <div className="flex flex-col justify-center z-0 items-center h-full w-1/2 leftdrop">
             <div>
               <img className='shadow-lg mb-2' src='/exampletest.png' width='400'/>
             </div>
@@ -72,7 +72,7 @@ export default function Home() {
             <button onClick={() => setPublish(!publish)} className='rounded bg-blue-500 hover:bg-blue-700 px-6 py-2 m-2 text-lg text-white'>Publish</button>
           </div>
         </div>
-        <div className="h-full w-1/2 flex flex-col items-center p-4">
+        <div className="h-full w-1/2 flex flex-col items-center p-4 overflow-scroll">
           <h1 className="text-xl font-bold">Responses</h1>
           <div className="flex flex-col w-full items-center justify-center">
             {genScores(studentScores)}
